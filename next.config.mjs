76b919cb2dea99241a1cf2f0d391945dade9f9c0
Zinoviy Lebed <127029646+zinuu4@users.noreply.github.com>
2023-10-04 20:14:24 +0300

    ✨ feat: configure svg sprites and implement custom Icon component (#3)
    * chore: configure svg sprites
    
    * feat: custom Icon component

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+import svg from '@neodx/svg/webpack';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.plugins.push(
+        svg({
+          root: 'src/shared/ui/icon/assets',
+          output: 'public',
+          metadata: 'src/shared/ui/icon/sprite.gen.ts',
+          resetColors: {
+            replaceUnknown: 'currentColor',
+          },
+        }),
+      );
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
---
+++ src/shared/ui/icon/index.ts
@@ -0,0 +1,2 @@
+export { Icon, type IconName, type IconProps } from './icon';
+export { SPRITES_META, type SpritesMap } from './sprite.gen';
